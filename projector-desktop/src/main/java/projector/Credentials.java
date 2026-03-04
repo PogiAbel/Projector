320cda2c5d2da58f73673122dab48711ac83ceb6
PogiAbel <pogonyiabel@gmail.com>
2026-03-04 16:15:00 +0100

    for proper build

--- projector-desktop/src/main/java/projector/Credentials.java
+++ projector-desktop/src/main/java/projector/Credentials.java
@@ -1,8 +1,10 @@
 package projector;
 
 public class Credentials {
-    public static final String DOMAIN = "localhost:8080";
+    // public static final String DOMAIN = "localhost:8080";
+    public static final String DOMAIN = "www.songpraise.com";
     public static final String WWW_DOMAIN = DOMAIN;
     public static final String BASE_URL = "http://" + WWW_DOMAIN; // if it's with https then on other devices will not work properly
     public static final String BASE_URL_S = "http://" + WWW_DOMAIN;
 }
+
